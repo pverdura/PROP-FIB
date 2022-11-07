bbe6c794a5ce7ae7bdddd3ff81d472fec1834621
jordi <jordi.palomera@estudiantat.upc.edu>
2022-11-07 17:46:57 +0100

    Implementació de cercaDoc.

--- src/Codi/Domini/CercaTitol.java
+++ src/Codi/Domini/CercaTitol.java
@@ -10,7 +10,17 @@ public class CercaTitol implements Cerca{
 
     @Override
     public void cercaDoc() {}
-    public ArrayList<Pair<String, String>> cercaDoc (String titol, HashMap<String, ArrayList<String>> documents) {
-        return null;
+    public ArrayList<Pair<String, String>> cercaDoc (String titol,  HashMap<String, ArrayList<String>> titolAutors,
+                                                     HashMap<String, ArrayList<String>> documents) {
+        ArrayList<Pair<String, String>> resultat = new ArrayList<Pair<String, String>>();
+
+        ArrayList<String> llistaAutors = titolAutors.get(titol);
+        int n = llistaAutors.size();
+        for (int i = 0; i < n; ++i) {
+            Pair<String, String> aux = new Pair<String, String>(titol, llistaAutors.get(i));
+            resultat.add(aux);
+        }
+
+        return resultat;
     }
 }
