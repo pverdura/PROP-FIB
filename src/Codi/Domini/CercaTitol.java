2efe95b2489ae947e23aa8a739016ea1c308cdd6
jordi <jordi.palomera@estudiantat.upc.edu>
2022-11-07 16:30:28 +0100

    Implementació de l'interfície Cerca.

--- src/Codi/Domini/CercaTitol.java
+++ src/Codi/Domini/CercaTitol.java
@@ -1,4 +1,16 @@
 package Codi.Domini;
 
-public class CercaTitol {
+
+import Codi.Util.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CercaTitol implements Cerca{
+
+    @Override
+    public void cercaDoc() {}
+    public ArrayList<Pair<String, String>> cercaDoc (String titol, HashMap<String, ArrayList<String>> documents) {
+        return null;
+    }
 }
--- src/Codi/Domini/CercaTitolAutor.java
+++ src/Codi/Domini/CercaTitolAutor.java
@@ -1,5 +1,14 @@
 package Codi.Domini;
 
-public class CercaTitolAutor extends Cerca{
+import Codi.Util.Pair;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CercaTitolAutor implements Cerca{
+    @Override
+    public void cercaDoc() {}
+    public Document cercaDoc (String titol, String autor, HashMap<String, ArrayList<String>> documents) {
+        return null;
+    }
 }
