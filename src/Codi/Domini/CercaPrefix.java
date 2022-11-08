864af7c46e7a1fad2e44e41de2808f9135efca08
juditsoi <judit.serna@estudiantat.upc.edu>
2022-11-08 11:41:10 +0100

    CercaPrefix actualitzades

--- src/Codi/Domini/CercaPrefix.java
+++ src/Codi/Domini/CercaPrefix.java
@@ -1,6 +1,7 @@
 package Codi.Domini;
 
 import Codi.Util.Pair;
+import Codi.Util.Trie;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,7 +11,7 @@ public class CercaPrefix implements Cerca{
     @Override
     public void cercaDoc() {}
 
-    public ArrayList<String> cercaDoc(String prefix){
+    public ArrayList<String> cercaDoc(String prefix, Trie<String> autors){
 
         ArrayList<String> resultat = new ArrayList<String>();
 
