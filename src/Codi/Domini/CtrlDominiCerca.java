b748f43703848ee9494f8b85bd1d09c2e3956131
juditsoi <judit.serna@estudiantat.upc.edu>
2022-11-08 11:58:49 +0100

    CtrlDominiCerca amb funcions static

--- src/Codi/Domini/CtrlDominiCerca.java
+++ src/Codi/Domini/CtrlDominiCerca.java
@@ -11,38 +11,32 @@ public class CtrlDominiCerca {
     public CtrlDominiCerca() {
     }
 
+    //Retorna
     ArrayList<Pair<String , String >> cercaAutor(String autor,  HashMap<String, ArrayList<String>> autorsTitols){
-        CercaAutor llistaAutors = new CercaAutor();
-        return llistaAutors.cercaDoc(autor, autorsTitols);
+        return CercaAutor.cercaDoc(autor, autorsTitols);
     }
 
     ArrayList<Pair<String , String >> cercaTitol(String titol,  HashMap<String, ArrayList<String>> titolsAutors){
-        CercaTitol llistaTitols = new CercaTitol();
-        return llistaTitols.cercaDoc(titol, titolsAutors);
+        return CercaTitol.cercaDoc(titol, titolsAutors);
     }
 
     Document cercaTitolAutor(String titol, String autor, HashMap<Pair<String, String>, Document> documents){
-        CercaTitolAutor llistaTitolsAutor = new CercaTitolAutor();
-        return llistaTitolsAutor.cercaDoc(titol, autor, documents);
+        return CercaTitolAutor.cercaDoc(titol, autor, documents);
     }
 
     ArrayList<String> cercaPrefix(String prefix, Trie<String> autors){
-        CercaPrefix llistaAutors = new CercaPrefix();
-        return llistaAutors.cercaDoc(prefix, autors);
+        return CercaPrefix.cercaDoc(prefix, autors);
     }
 
     ArrayList<Pair<String , String >> cercaSemblant(Document document, int k, HashMap<Pair<String,String>, Document> documents, Boolean tipus){
-        CercaSemblant llistaDocuments = new CercaSemblant();
-        return llistaDocuments.cercaDoc(document, k, documents, tipus);
+        return CercaSemblant.cercaDoc(document, k, documents, tipus);
     }
 
     ArrayList<Pair<String , String >> cercaParaules(ArrayList<String> paraules, int k, HashMap<String,ArrayList<String>> paraulesDocuments, Boolean tipus){
-        CercaParaules llistaDocuments = new CercaParaules();
-        return llistaDocuments.cercaDoc(paraules, k, paraulesDocuments, tipus);
+        return CercaParaules.cercaDoc(paraules, k, paraulesDocuments, tipus);
     }
 
     int cercaBooleana(int a){
-        CercaBooleana llistaDocuments = new CercaBooleana();
-        return llistaDocuments.cercaDoc(a);
+        return CercaBooleana.cercaDoc(a);
     }
 }
