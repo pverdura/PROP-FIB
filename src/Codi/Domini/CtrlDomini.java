3ed6cea31415fad3731ff70bb65013b5013bf284
pol.verdura <pol.verdura@estudiantat.upc.edu>
2022-11-08 10:52:01 +0100

    Creadora inicialitzada i funcions CtrlDOminiDocument (creaDocument ~ setPath)

--- src/Codi/Domini/CtrlDomini.java
+++ src/Codi/Domini/CtrlDomini.java
@@ -3,12 +3,16 @@
 package Codi.Domini;
 
 import Codi.Util.Pair;
+import Codi.Util.Trie;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CtrlDomini {
 
+    ///////////////////////////////////////////////////////////
+    ///                      ATRIBUTS                       ///
+    ///////////////////////////////////////////////////////////
     private HashMap<Pair<String,String>,Document> Documents; // Estructura on es guarden els documents
     private Trie<String> Autors; // Estructura on es guarden els autors (serveix per trobar el prefix)
     private HashMap<String,ArrayList<String>> DocumentsAutor; // Estructura on es guarden els títols dels documents creats per un autor
@@ -16,55 +20,111 @@ public class CtrlDomini {
     private HashMap<String,ArrayList<Pair<String,String>>> Paraules; // Estructura on es guarden els documents que contenen la paraula
     private ArrayList<ExpressioBooleana> ExpressionsBooleanes; // Estructura on es guarden totes les expression booleanes
 
-    public CtrlDomini() {}
+    private CtrlDominiExprBool CDeb;    // Agregació del controlador d'expressió booleana
+
+    private CtrlDominiDocument CDdoc;   // Agregació del controlador de document
+
+    private CtrlDominiCerca CDcer;      // Agregació del controlador de cerca
+
+
+    ///////////////////////////////////////////////////////////
+    ///                      CREADORES                      ///
+    ///////////////////////////////////////////////////////////
+
+    // Crea i inicialitza els altres controladors
+    public CtrlDomini() {
+        CDeb = new CtrlDominiExprBool();
+        CDdoc = new CtrlDominiDocument();
+        CDcer = new CtrlDominiCerca();
+    }
+
+
+    ///////////////////////////////////////////////////////////
+    ///                       GETTERS                       ///
+    ///////////////////////////////////////////////////////////
 
     /* Pre: -
      * Post: Retorna un llistat de tots els Documents del sistema
      */
-    protected HashMap<Pair<String,String>,Document> getDocuments() {
+    public HashMap<Pair<String,String>,Document> getDocuments() {
         return Documents;
     }
 
     /* Pre: -
      * Post: Retorna un Arbre amb tots els autors del sistema
      */
-    protected Trie<String> getAutors() {
+    public Trie<String> getAutors() {
         return Autors;
     }
 
     /* Pre: -
      * Post: Retorna un llistat dels títols de cada autor
      */
-    protected HashMap<String,ArrayList<String>> getDocumentsAutor() {
+    public HashMap<String,ArrayList<String>> getDocumentsAutor() {
         return DocumentsAutor;
     }
 
     /* Pre: -
      * Post: Retorna un llistat dels autors que han creat cada títol
      */
-    protected HashMap<String,ArrayList<String>> getTítolAutors() {
+    public HashMap<String,ArrayList<String>> getTítolAutors() {
         return TitolAutors;
     }
 
     /* Pre: -
      * Post: Retorna les paraules de tots els documents i on apareix
      */
-    protected HashMap<String,ArrayList<Pair<String,String>>> getParaules() {
+    public HashMap<String,ArrayList<Pair<String,String>>> getParaules() {
         return Paraules;
     }
 
     /* Pre: -
      * Post: Retorna totes les expressions booleanes del sistema
      */
-    protected ArrayList<ExpressioBooleana> getExpressionsBooleanes() {
+    public ArrayList<ExpressioBooleana> getExpressionsBooleanes() {
         return ExpressionsBooleanes;
     }
+
+
+    ///////////////////////////////////////////////////////////
+    ///            FUNCIONS CTRL_DOMINI_DOCUMENT            ///
+    ///////////////////////////////////////////////////////////
 
     /* Afegeix un nou document amb paràmetres (títol, autor)
      * Pre: Els String titol i autor no són buits
      * Post: True si s'ha creat el document, false si el document identificat per {titol,autor} ja existeix en el sistema
      */
     public Boolean creaDocument(String titol, String autor) {
-        return null;
+        CDdoc.creaDocument(titol,autor,Documents);
+        return true;
     }
+
+    public Boolean setContingut(String titol, String autor, String contingut) {
+        CDdoc.setContingut(titol,autor,contingut,Documents);
+        return true;
+    }
+
+    public String getContingut(String titol, String autor) {
+        return CDdoc.getContingut(titol,autor,Documents);
+    }
+
+    public Boolean setPath(String titol, String autor, String path) {
+        CDdoc.setPath(titol,autor,path,Documents);
+        return true;
+    }
+
+    //public Boolean getPath(String)
+
+    ///////////////////////////////////////////////////////////
+    ///             FUNCIONS CTRL_DOMINI_CERCA              ///
+    ///////////////////////////////////////////////////////////
+
+    //...
+
+
+    ///////////////////////////////////////////////////////////
+    ///           FUNCIONS CTRL_DOMINI_EXPR.BOOL.           ///
+    ///////////////////////////////////////////////////////////
+
+    //
 }
