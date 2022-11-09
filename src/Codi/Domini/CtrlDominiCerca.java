b0a9798c827253ffa8ece2836dc144380aabd9df
juditsoi <judit.serna@estudiantat.upc.edu>
2022-11-09 08:31:42 +0100

    CtrlDominiCerca act

--- src/Codi/Domini/CtrlDominiCerca.java
+++ src/Codi/Domini/CtrlDominiCerca.java
@@ -12,31 +12,31 @@ public class CtrlDominiCerca {
     }
 
     //Retorna
-    ArrayList<Pair<String , String >> cercaAutor(String autor,  HashMap<String, ArrayList<String>> autorsTitols){
+    public ArrayList<Pair<String , String >> cercaAutor(String autor,  HashMap<String, ArrayList<String>> autorsTitols){
         return CercaAutor.cercaDoc(autor, autorsTitols);
     }
 
-    ArrayList<Pair<String , String >> cercaTitol(String titol,  HashMap<String, ArrayList<String>> titolsAutors){
+    public ArrayList<Pair<String , String >> cercaTitol(String titol,  HashMap<String, ArrayList<String>> titolsAutors){
         return CercaTitol.cercaDoc(titol, titolsAutors);
     }
 
-    Document cercaTitolAutor(String titol, String autor, HashMap<Pair<String, String>, Document> documents){
+    public Document cercaTitolAutor(String titol, String autor, HashMap<Pair<String, String>, Document> documents){
         return CercaTitolAutor.cercaDoc(titol, autor, documents);
     }
 
-    ArrayList<String> cercaPrefix(String prefix, Trie<String> autors){
+    public ArrayList<String> cercaPrefix(String prefix, Trie<String> autors){
         return CercaPrefix.cercaDoc(prefix, autors);
     }
 
-    ArrayList<Pair<String , String >> cercaSemblant(Document document, int k, HashMap<Pair<String,String>, Document> documents, Boolean tipus){
-        return CercaSemblant.cercaDoc(document, k, documents, tipus);
+    public ArrayList<Pair<String , String >> cercaSemblant(Document document, int k){
+        return CercaSemblant.cercaDoc(document, k);
     }
 
-    ArrayList<Pair<String , String >> cercaParaules(ArrayList<String> paraules, int k, HashMap<String,ArrayList<String>> paraulesDocuments, Boolean tipus){
-        return CercaParaules.cercaDoc(paraules, k, paraulesDocuments, tipus);
+    public ArrayList<Pair<String , String >> cercaParaules(ArrayList<String> paraules, int k){
+        return CercaParaules.cercaDoc(paraules, k);
     }
 
-    int cercaBooleana(int a){
+    public int cercaBooleana(int a){
         return CercaBooleana.cercaDoc(a);
     }
 }
