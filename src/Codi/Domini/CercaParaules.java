ea1b16819bbb4883d62f60017f5cc7ce43293bf4
pol.verdura <pol.verdura@estudiantat.upc.edu>
2022-11-09 12:03:10 +0100

    Modificació per a aprofitar el codi en la cercaParaules i cercaSemblant

--- src/Codi/Domini/CercaParaules.java
+++ src/Codi/Domini/CercaParaules.java
@@ -25,12 +25,28 @@ public class CercaParaules implements Cerca {
      */
     public static ArrayList<Pair<String,String>> cercaDoc(ArrayList<String> paraules, int k, HashMap<String,ArrayList<Pair<String,String>>> DocumentsParaules,
                                                    HashMap<Pair<String,String>,Document> Documents) {
+        // Array on es posaran els identificadors dels documents que cerquem
+        ArrayList<Pair<String,String>> docs = new ArrayList<Pair<String,String>>();
+
+        // Array on guardem les paraules de la query i el nombre de document on apareix
+        ArrayList<Pair<String,Double>> paraulesIDF = new ArrayList<Pair<String,Double>>();
+
+        int N = Documents.size();   // Nombre de documents
+
+        for (String p : paraules) {   // Afegim en l'array les paraules i el seu idf
+            int aparicions = DocumentsParaules.get(p).size();
+            double idf = Math.log((double) N/aparicions)/Math.log(2);   // Calculem log2(#Docs/#DocsApareix)
+            Pair<String,Double> elem = new Pair<String,Double>(p,idf);
+            paraulesIDF.add(elem);
+        }
+
         for(Pair<String,String> idConsulta : Documents.keySet()) {  // Iterem tots els Documents del sistema
-            // HashMap<String,Integer> paraulesDConsulta = Documents.get(idConsulta).getAparicions();
-            int totalParaulesConsultat = 0;
+            Document Dcons = Documents.get(idConsulta); // Obtenim el document X
+
+            Double calc = EspaiVec.calculaTF_IDF(paraulesIDF, Dcons); // Calculem el ft-idf del document X
 
         }
 
-        return null;
+        return docs;
     }
 }
--- src/Codi/Domini/CercaSemblant.java
+++ src/Codi/Domini/CercaSemblant.java
@@ -20,53 +20,37 @@ public class CercaSemblant implements Cerca {
     ///                      FUNCIONS                       ///
     ///////////////////////////////////////////////////////////
 
-    /* Pre: El contingut del document D no és buit, k >= 0
+    /* S'obtenen els k documents més semblants al document D
+     * Pre: El contingut del document D no és buit, k >= 0
      * Post: Un llistat de k Documents (títol,autor), més semblants al document D
      */
     public static ArrayList<Pair<String,String>> cercaDoc(Document D, int k,  HashMap<String,ArrayList<Pair<String,String>>> DocumentsParaules,
                                                    HashMap<Pair<String,String>, Document> Documents) {
-        ArrayList<Pair<String,String>> docs = new ArrayList<Pair<String,String>>(k);    // Array on es posaran els identificadors dels documents que cerquem
-        HashMap<String,Integer> paraulesD = D.getAparicions();      // Array on guardem les paraules rellevants del document D (sense repetides)
+        // Array on es posaran els identificadors dels documents que cerquem
+        ArrayList<Pair<String,String>> docs = new ArrayList<Pair<String,String>>();
 
-        HashMap<Pair<String,String>,Double> taula_tfidf = new HashMap<Pair<String,String>,Double> ();
+        // Array on guardem les paraules del document D i el nombre seu idf
+        ArrayList<Pair<String,Double>> paraulesIDF = new ArrayList<Pair<String,Double>>();
+
+        int N = Documents.size();   // Nombre de documents
+
+        for (String p : D.getAparicions().keySet()) {   // Afegim en l'array les paraules i el seu idf
+            int aparicions = DocumentsParaules.get(p).size();
+            double idf = Math.log((double) N/aparicions)/Math.log(2);   // Calculem log2(#Docs/#DocsApareix)
+            Pair<String,Double> elem = new Pair<String,Double>(p,idf);
+            paraulesIDF.add(elem);
+        }
 
         for(Pair<String,String> idConsulta : Documents.keySet()) {  // Iterem tots els Documents del sistema
-            if (!idConsulta.getFirst().equals(D.getTitol()) || !idConsulta.getSecond().equals(D.getAutor())) {
-                // Mirem que el Document que iterem no sigui el que estiguem utilitzant per fer la cerca
-                HashMap<String, Integer> paraulesDCons = Documents.get(idConsulta).getAparicions(); // Obtenim les paraules del document X consultat
-                HashMap<String, Double> taula_tf = new HashMap<String, Double>(); // Map on guardem el tf de cada paraula
-                HashMap<String, Double> taula_idf = new HashMap<String, Double>(); // Map on guardem el idf de cada paraula
-                int numParaules = 0;
+            Document Dcons = Documents.get(idConsulta); // Obtenim el document X
 
-                for (String paraulaDCons : paraulesDCons.keySet()) { // Contem el nombre total de paraules
-                    numParaules += paraulesDCons.get(paraulaDCons);
-                }
+            // Mirem que el Document que iterem no sigui el que estiguem utilitzant per fer la cerca
+            if(!idConsulta.getFirst().equals(D.getTitol()) || !idConsulta.getSecond().equals(D.getAutor())) {
+                Double calc = EspaiVec.calculaTF_IDF(paraulesIDF, Dcons); // Calculem el ft-idf del document X
 
-                for (String paraulaD : paraulesD.keySet()) { // Calculem el tf de les paraules del document X
-                    if (paraulesDCons.containsKey(paraulaD)) {   // Conté la paraula
-                        Double tf = (double) paraulesDCons.get(paraulaD)/numParaules;    // El nombre d'aparicions de la paraulaD en el document X
-                        taula_tf.put(paraulaD, tf);  // Afegim la paraula amb el càlcul tf corresponent
-                    }
-                    else {  // La paraula no existeix en el document
-                        taula_tf.put(paraulaD, 0.0);
-                    }
-                    int N = DocumentsParaules.get(paraulaD).size(); // Obtenim en quants documents aparareix la paraulaD
-                    Double idf = Math.log((double) Documents.size()/N);//log2(Documents.size() / N); // Calculem el idf de cada paraula
-                    taula_idf.put(paraulaD, idf); // Afegim la paraula amb el seu idf corresponent
-                }
-                double tf_idf = 0;
-                for (String paraula : taula_tf.keySet()) { // calculem el tf_idf del document X consultat
-                    double a = taula_tf.get(paraula);
-                    double b = taula_idf.get(paraula);
-                    tf_idf += (a * b);
-                }
-                taula_tfidf.put(idConsulta, tf_idf); // taula amb els tf-idf de tots els documents
             }
         }
-        return null;
-    }
 
-    private static Double log2(int n) {
-        return Math.log(n)/Math.log(2);
+        return docs;
     }
 }
