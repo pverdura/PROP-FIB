1feaaefed7a92a6e4b3b3bca867cbb3d6c04c6bc
jordi <jordi.palomera@estudiantat.upc.edu>
2022-11-10 22:54:01 +0100

    Creació i ús de les excepcions DocumentInexistentException i DocumentJaExisteixException

---
+++ src/Codi/Util/DocumentInexistentException.java
@@ -0,0 +1,15 @@
+package Codi.Util;
+
+public class DocumentInexistentException extends NullPointerException {
+    String titol, autor;
+
+    public DocumentInexistentException (String titol, String autor) {
+        super("Error: el document de nom: " + titol + " creat per l'autor: " + autor + " no existeix.");
+        this.titol = titol;
+        this.autor = autor;
+    }
+
+    public String toString () {
+        return "Error: el document de nom: " + this.titol + " creat per l'autor: " + this.autor + " no existeix.";
+    }
+}
