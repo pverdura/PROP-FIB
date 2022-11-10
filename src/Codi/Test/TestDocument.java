f24c7873613f35c3b0c073da93a085ec04fb768f
jordi <jordi.palomera@estudiantat.upc.edu>
2022-11-10 17:21:22 +0100

    TestDocument.java

--- src/Codi/Test/TestDocument.java
+++ src/Codi/Test/TestDocument.java
@@ -2,28 +2,9 @@ package Codi.Test;
 
 import java.util.HashMap;
 
+
 public class TestDocument {
-
-    static void print (HashMap<String, Integer> h) {
-        for (HashMap.Entry<String, Integer> set : h.entrySet()) {
-
-            System.out.println(set.getKey() + " = "
-                    + set.getValue());
-        }
-    }
     public static void main (String[] args) {
-
-    }
-
-    public void testConstructorBuit () {
-
-    }
-
-    public void testConstructor2 () {
-
-    }
-
-    public void testConstructor3 () {
 
     }
 }
