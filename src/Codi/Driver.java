bd9af9bc0dcb7a5d61ec19c1a9d17423c8c61b77
jordi <jordi.palomera@estudiantat.upc.edu>
2022-11-11 15:44:42 +0100

    Driver principal.

--- src/Codi/Driver.java
+++ src/Codi/Driver.java
@@ -1,11 +1,13 @@
 package Codi;
 
 import Codi.Domini.CtrlDomini;
+import Codi.Util.TipusExtensio;
+import Codi.Util.TipusOrdenacio;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.lang.reflect.Array;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -56,6 +58,9 @@ public class Driver {
                     dv.testModificarExpressioBooleana();
                     break;
                 case 11:
+                    dv.testEliminarExpressioBooleana();
+                    break;
+                case 12:
                     running = false;
                     break;
                 default:
@@ -77,7 +82,8 @@ public class Driver {
         System.out.println("8. Veure expressions booleanes");
         System.out.println("9. Crear expressió booleana");
         System.out.println("10. Modificar expressió booleana");
-        System.out.println("11. Sortir");
+        System.out.println("11. Eliminar expressió booleana");
+        System.out.println("12. Sortir");
     }
 
     void llegirStopWords () {
@@ -105,30 +111,259 @@ public class Driver {
         }
     }
     void testCrearDocument () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Títol del document:\t");
+        String nom = sc.nextLine();
+        System.out.println("Autor del document:\t");
+        String autor = sc.nextLine();
 
+        try {
+            cd.creaDocument(nom, autor);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
     void testEliminarDocument () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Títol del document:\t");
+        String nom = sc.nextLine();
+        System.out.println("Autor del document:\t");
+        String autor = sc.nextLine();
 
+        try {
+            cd.eliminaDocument(nom, autor);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
     void testModificarDocument () {
+        //títol, autor, contingut, path, extensió,
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Títol del document:\t");
+        String nom = sc.nextLine();
+        System.out.println("Autor del document:\t");
+        String autor = sc.nextLine();
 
+        System.out.println("Què vols modificar?");
+        System.out.println("1. Títol");
+        System.out.println("2. Autor");
+        System.out.println("3. Contingut");
+        System.out.println("4. Path");
+        System.out.println("5. Extensió");
+
+        int op = sc.nextInt();
+        switch (op) {
+            case 1:
+                System.out.println("Nou títol del document:\t");
+                String nomNou = sc.nextLine();
+
+                try {
+                    cd.setTitol(nom, autor, nomNou);
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                }
+                break;
+            case 2:
+                System.out.println("Nou autor del document:\t");
+                String autorNou = sc.nextLine();
+
+                try {
+                    cd.setAutor(nom, autor, autorNou);
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                }
+                break;
+            case 3:
+                System.out.println("Nou contingut del document:\t");
+                String contingutNou = sc.nextLine();
+
+                try {
+                    cd.setContingut(nom, autor, contingutNou);
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                }
+                break;
+            case 4:
+                System.out.println("Nova path del document:\t");
+                String novaPath = sc.nextLine();
+
+                try {
+                    cd.setPath(nom, autor, novaPath);
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                }
+                break;
+            case 5:
+                System.out.println("Nova extensió del document:\t");
+                System.out.println("1. TXT");
+                System.out.println("2. XML");
+                System.out.println("2. BOL");
+                int aux = sc.nextInt();
+                TipusExtensio novaExtensio;
+                if (aux == 1) novaExtensio = TipusExtensio.TXT;
+                else if (aux == 2) novaExtensio = TipusExtensio.XML;
+                else  novaExtensio = TipusExtensio.BOL;
+
+                try {
+                    cd.setExtensio(nom, autor, novaExtensio);
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                }
+                break;
+            default:
+                System.out.println("Error: opció no vàlida.");
+                break;
+        }
     }
     void testVeureDocument () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Títol del document:\t");
+        String nom = sc.nextLine();
+        System.out.println("Autor del document:\t");
+        String autor = sc.nextLine();
 
+        try {
+            System.out.println("Títol: "+nom);
+            System.out.println("Autor: "+autor);
+            System.out.println("Path: "+cd.getPath(nom, autor));
+            System.out.println("Extensió: "+cd.getExtensio(nom, autor));
+            System.out.println("Contingut: "+cd.getContingut(nom, autor));
+            System.out.println("Pes: "+cd.getPes(nom, autor));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
     void testResumDocuments () {
+        ArrayList<SimpleEntry<String, String>> docs = cd.getDocuments();
 
+        for (SimpleEntry<String, String> se : docs) {
+            System.out.println(se.getKey() + " " + se.getValue());
+        }
     }
     void testCercaDocuments () {
+        //cerca documents
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quina cerca vols fer?");
+        System.out.println("1. Per títol");
+        System.out.println("2. Per prefix d'autor");
+        System.out.println("3. Per paraules");
+        System.out.println("4. Per expressió booleana");
+        System.out.println("5. Per document semblant");
+        System.out.println("6. Per autor");
 
+        int op = sc.nextInt();
+
+        System.out.println("Com ho vols ordenar?");
+        System.out.println("1. Alfabèticament ascendent");
+        System.out.println("2. Alfabèticament descendent");
+        System.out.println("3. Per pes ascendent");
+        System.out.println("4. Per pes ascendent");
+        int ord = sc.nextInt();
+        TipusOrdenacio tipusOrdenacio;
+        if (ord == 1) tipusOrdenacio = TipusOrdenacio.ALFABETIC_ASCENDENT;
+        else if (ord == 2) tipusOrdenacio = TipusOrdenacio.ALFABETIC_DESCENDENT;
+        else if (ord == 3) tipusOrdenacio = TipusOrdenacio.PES_ASCENDENT;
+        else tipusOrdenacio = TipusOrdenacio.PES_DESCENDENT;
+
+        ArrayList<SimpleEntry<String, String>> res = new ArrayList<>();
+        String s;
+
+        switch (op) {
+            case 1:
+                System.out.println("Títol:\t");
+                s = sc.next();
+                res = cd.cercaTitol(s, tipusOrdenacio);
+
+                break;
+            case 2:
+                System.out.println("Prefix d'autor:\t");
+                s = sc.next();
+                ArrayList<String> res2 = cd.cercaPrefix(s, tipusOrdenacio);
+                for (String se : res2) {
+                    System.out.println(se);
+                }
+                break;
+            case 3:
+                System.out.println("Paraules:\t");
+                s = sc.nextLine();
+                System.out.println("Nombre de documents:\t");
+                int k = sc.nextInt();
+                res = cd.cercaParaules(s, k, tipusOrdenacio);
+
+                break;
+            case 4:
+                System.out.println("Expressió booleana:\t");
+                s = sc.next();
+                res = cd.cercaBooleana(s, tipusOrdenacio);
+
+                break;
+            case 5:
+                System.out.println("Títol document:\t");
+                String nom = sc.next();
+                System.out.println("Autor document:\t");
+                String autor = sc.next();
+                System.out.println("Nombre de documents:\t");
+                k = sc.nextInt();
+
+                res = cd.cercaSemblant(nom, autor, k, tipusOrdenacio);
+
+                break;
+            case 6:
+                System.out.println("Autor:\t");
+                s = sc.next();
+                res = cd.cercaAutor(s, tipusOrdenacio);
+
+                break;
+                break;
+            default:
+                System.out.println("Error: opció no vàlida.");
+                break;
+        }
+
+        for (SimpleEntry<String, String> se : res) {
+            System.out.println(se.getKey() + " " + se.getValue());
+        }
     }
     void testVeureExpressionsBooleanes () {
+        ArrayList<String> exprs = cd.getExpressionsBooleanes();
 
+        for (String se : exprs) {
+            System.out.println(se);
+        }
     }
     void testCrearExpressioBooleana () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Expressió booleana:\t");
+        String expr = sc.nextLine();
 
+        try {
+            cd.creaExpressioBool(expr);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
     void testModificarExpressioBooleana () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Expressió booleana a modificar:\t");
+        String expr = sc.nextLine();
+        System.out.println("Nova booleana a modificar:\t");
+        String exprNova = sc.nextLine();
 
+        try {
+            cd.modificaExpressioBool(expr, exprNova);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+    void testEliminarExpressioBooleana () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Expressió booleana a eliminar:\t");
+        String expr = sc.nextLine();
+
+        try {
+            cd.eliminaExpressioBool(expr);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
