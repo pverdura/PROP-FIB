0a81786376773ec86992485bdd68a7c668db0948
juditsoi <judit.serna@estudiantat.upc.edu>
2022-11-13 00:51:20 +0100

    Tests actualitzats Judit

--- src/Codi/Tests/TrieTest.java
+++ src/Codi/Tests/TrieTest.java
@@ -10,15 +10,12 @@ import static org.junit.Assert.*;
 
 public class TrieTest {
     Trie trie;
-    String autor;
+    String autor1;
     String autor2;
     String autor3;
 
     @Before
-    public void init(){
-        trie = new Trie<>();
-        assertNotNull(trie);
-    }
+    public void init(){ trie = new Trie<>();  }
 
     @Test
     public void testConstructora() {
@@ -27,58 +24,83 @@ public class TrieTest {
 
     @Test
     public void testAfegirConteCorrecte() {
-        autor = "Brandon Sanderson";
-        trie.afegir(autor);
-        assertTrue(trie.conteParaula(autor));
+        autor1 = "Brandon Sanderson";
+        trie.afegir(autor1);
+        assertTrue(trie.conteParaula(autor1));
     }
 
     @Test
     public void testAfegirConteCorrecte2() {
-        autor = "Brandon Sanderson";
+        autor1 = "Brandon Sanderson";
         autor2 = "Brandon Sanderso";
-        trie.afegir(autor);
+        trie.afegir(autor1);
         assertFalse(trie.conteParaula(autor2));
     }
 
     @Test
     public void testEsborrarCorrecte() {
-        autor = "Brandon Sanderson";
-        trie.afegir(autor);
-        trie.esborrar(autor);
-        assertFalse(trie.conteParaula(autor));
+        autor1 = "Brandon Sanderson";
+        trie.afegir(autor1);
+        trie.esborrar(autor1);
+        assertFalse(trie.conteParaula(autor1));
     }
 
     @Test
     public void testEsborrarCorrecte2() {
-        autor = "Brandon Sanderson";
+        autor1 = "Brandon Sanderson";
         autor2 = "Brandon Sanderso";
-        trie.afegir(autor);
+        trie.afegir(autor1);
         trie.afegir(autor2);
-        trie.esborrar(autor);
-        assertTrue(trie.conteParaula(autor2) && !trie.conteParaula(autor));
+        trie.esborrar(autor1);
+        assertTrue(trie.conteParaula(autor2) && !trie.conteParaula(autor1));
+    }
+
+    @Test
+    public void testAfegirEsborrarCorrecte2() {
+        autor1 = "Brandon Sanderson";
+        trie.afegir(autor1);
+        trie.afegir(autor1);
+        trie.esborrar(autor1);
+        assertFalse(trie.conteParaula(autor1));
     }
 
     @Test
     public void testPrefixCorrecte() {
-        autor = "Brandon Sanderson";
+        autor1 = "Brandon Sanderson";
         autor2 = "Brook Crane";
         autor3 = "Borja Sander";
-        trie.afegir(autor);
+        trie.afegir(autor1);
         trie.afegir(autor2);
         trie.afegir(autor3);
         ArrayList<String> resultat = trie.getParaules("Br");
         ArrayList<String> res_esperat = new ArrayList<>();
-        res_esperat.add(autor);
+        res_esperat.add(autor1);
         res_esperat.add(autor2);
         assertEquals(res_esperat, resultat);
     }
 
     @Test
     public void testPrefixCorrecte2() {
-        autor = "Brandon Sanderson";
+        autor1 = "Brandon Sanderson";
         autor2 = "Brook Crane";
         autor3 = "Borja Sander";
-        trie.afegir(autor);
+        trie.afegir(autor1);
+        trie.afegir(autor2);
+        trie.afegir(autor3);
+        ArrayList<String> resultat = trie.getParaules("B");
+        ArrayList<String> res_esperat = new ArrayList<>();
+        res_esperat.add(autor1);
+        res_esperat.add(autor2);
+        res_esperat.add(autor3);
+        assertEquals(res_esperat, resultat);
+    }
+
+    @Test
+    public void testPrefixCorrecte3() {
+        autor1 = "Brandon Sanderson";
+        autor2 = "Brook Crane";
+        autor3 = "Borja Sander";
+        trie.afegir(autor1);
         trie.afegir(autor2);
         trie.afegir(autor3);
         ArrayList<String> resultat = trie.getParaules("Bon");
