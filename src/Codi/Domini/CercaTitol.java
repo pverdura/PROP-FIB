7a7579cbd222020182a6d773ce3ca3caa40ec838
jordi <jordi.palomera@estudiantat.upc.edu>
2022-11-13 14:05:31 +0100

    Test de Cerca Títol acabat.

--- src/Codi/Domini/CercaTitol.java
+++ src/Codi/Domini/CercaTitol.java
@@ -1,6 +1,6 @@
 package Codi.Domini;
 
-import Codi.Excepcions.DocumentInexistentException;
+import Codi.Excepcions.TitolNoExisteixException;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
@@ -8,10 +8,10 @@ import java.util.HashMap;
 
 public class CercaTitol implements Cerca {
 
-    public static ArrayList<SimpleEntry<String, String>> cercaDoc (String titol,  HashMap<String, ArrayList<String>> titolAutors) throws DocumentInexistentException {
+    public static ArrayList<SimpleEntry<String, String>> cercaDoc (String titol,  HashMap<String, ArrayList<String>> titolAutors) throws TitolNoExisteixException {
         ArrayList<SimpleEntry<String, String>> resultat = new ArrayList<>();
 
-        if (!titolAutors.containsKey(titol)) throw new DocumentInexistentException(titol, "[QUALSEVOL]");
+        if (!titolAutors.containsKey(titol)) throw new TitolNoExisteixException(titol);
 
         ArrayList<String> llistaAutors = titolAutors.get(titol);
 
