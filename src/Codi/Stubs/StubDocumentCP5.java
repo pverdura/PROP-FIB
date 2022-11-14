7889298963fc02293861b3692ac07cea49f68733
pol.verdura <pol.verdura@estudiantat.upc.edu>
2022-11-14 16:16:58 +0100

    CercaSemblantTest

---
+++ src/Codi/Stubs/StubDocumentCP5.java
@@ -0,0 +1,18 @@
+package Codi.Stubs;
+
+import Codi.Domini.Document;
+
+import java.util.HashMap;
+
+public class StubDocumentCP5 extends Document {
+    @Override
+    public HashMap<String, Integer> getAparicions() {
+        System.out.println("Crida al metode getAparicions() sense paràmetres");
+
+        HashMap<String, Integer> aux = new HashMap<String, Integer>();
+        aux.put("pikachu",1);
+        aux.put("gos",1);
+
+        return aux;
+    }
+}
