268dcc16867fe7632d0cda76f38f58c4665ae7a4
jordi <jordi.palomera@estudiantat.upc.edu>
2022-12-07 20:13:08 +0100

    u

--- src/Codi/Presentacio/CtrlPresentacio.java
+++ src/Codi/Presentacio/CtrlPresentacio.java
@@ -3,14 +3,23 @@ package Codi.Presentacio;
 import Codi.Domini.CtrlDomini;
 import Codi.Excepcions.ExpressioBooleanaInexistentException;
 import Codi.Excepcions.ExpressioBooleanaJaExistentException;
+import Codi.Util.TipusOrdenacio;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 
 public class CtrlPresentacio {
     private CtrlDomini ctrlDomini;
     private ViewMenuPrincipal viewMenuPrincipal;
     private ViewGestioExprBool viewGestioExprBool;
 
+    private ArrayList<SimpleEntry<String, String>> resultatActual1;
+
     public CtrlPresentacio () {
         ctrlDomini = new CtrlDomini();
+
+        resultatActual1 = new ArrayList<>();
+
         viewMenuPrincipal = new ViewMenuPrincipal(this);
         viewGestioExprBool = new ViewGestioExprBool(this);
     }
@@ -53,8 +62,18 @@ public class CtrlPresentacio {
         }
     }
 
-    public boolean cercaBooleana () {
-        return true;
+    public ArrayList<String> getExpressionsBooleanes () {
+        return ctrlDomini.cercaAllExpressionsBool(TipusOrdenacio.ALFABETIC_ASCENDENT);
+    }
+
+    public boolean cercaBooleana (String expr) {
+        try {
+            resultatActual1 = ctrlDomini.cercaBooleana(expr, viewMenuPrincipal.getTipusOrdenacio());
+            viewMenuPrincipal.actualitzarResultat(resultatActual1);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public boolean cercaTitol () {
@@ -66,6 +85,17 @@ public class CtrlPresentacio {
     }
 
     public boolean cercaTitolAutor () {
+        return true;
+    }
+
+    public boolean cercaPrefix () {
+        return true;
+    }
+
+    public boolean cercaParaules () {
+        return true;
+    }
+    public boolean cercaSemblant () {
         return true;
     }
 }
