fa29d642d092ac1cfb358a7e796a11c35b249700
pol.verdura <pol.verdura@estudiantat.upc.edu>
2022-12-12 22:56:02 +0100

    vista ajuda

--- src/Codi/Presentacio/ViewAjuda.java
+++ src/Codi/Presentacio/ViewAjuda.java
@@ -1,15 +1,57 @@
 package Codi.Presentacio;
 
 import javax.swing.*;
-import javax.swing.text.View;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ViewAjuda {
-    private JTextPane siVolsFerUnaTextPane;
-    private JPanel panel1;
-    private JButton tancarButton;
-    private JFrame frame1;
+    private JPanel panell;
+    private JButton tancar_button;
+    private JLabel text_ajuda;
+    private JFrame frame;
+
+    public ViewAjuda() {
+        inicialitzarComponents();
+        configurarVista();
+        assignarListeners();
+    }
+
+    private String setText() {
+        return "Text d'ajuda";
+    }
+
+    private void inicialitzarComponents() {
+        frame = new JFrame("Ajuda");
+        panell = new JPanel();
+        tancar_button = new JButton("Tancar");
+        text_ajuda = new JLabel(setText());
+    }
+
+    private void configurarVista() {
+        frame.setLayout(new BorderLayout());
+        frame.add(panell,BorderLayout.CENTER);
+
+        BorderLayout border = new BorderLayout();
+        panell.setLayout(border);
+        panell.add(tancar_button,BorderLayout.EAST);
+        panell.add(text_ajuda,BorderLayout.CENTER);
+
+        frame.setMinimumSize(new Dimension(400,400));
+        frame.setPreferredSize(frame.getMinimumSize());
+        frame.setLocationRelativeTo(null);
+    }
+
+    private void assignarListeners() {
+        tancar_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+            }
+        });
+    }
 
     public void ferVisible() {
-        frame1.setVisible(true);
+        frame.setVisible(true);
     }
 }
