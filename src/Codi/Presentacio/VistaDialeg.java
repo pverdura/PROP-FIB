1a0a8ad077079d3356a2092b5e01d8f2cf0a644d
Pau Vilaro Lozano <pau.vilaro@gracia.lasalle.cat>
2022-12-15 19:58:25 +0100

    javaDocs de les meves classes de presentacio fetes

--- src/Codi/Presentacio/VistaDialeg.java
+++ src/Codi/Presentacio/VistaDialeg.java
@@ -3,8 +3,20 @@ package Codi.Presentacio;
 import javax.swing.*;
 import java.util.AbstractMap.SimpleEntry;
 
+/**
+ * Classe que gestiona diferents dialegs necessaris dins les vistes
+ * @author PauVi
+ */
 public class VistaDialeg {
 
+
+    /**
+     * Funcio estatica que crea un dialeg amb un camp a omplir
+     * @param titol Titol del dialeg
+     * @param label Nom del camp que voldrem omplir
+     * @param options Opcions resultants a mostrar
+     * @return Retorna els resultats dels camps a omplir
+     */
     public static String inputDialog(String titol, String label, Object[] options) {
 
         //Crear panel input
@@ -22,6 +34,15 @@ public class VistaDialeg {
         return null;
     }
 
+    /**
+     * Funcio estatica que crea un dialeg amb dos camps a omplir
+     * @param titol Titol del dialeg
+     * @param label1 Nom del primer camp per omplir
+     * @param label2 Nom del segon camp per omplir
+     * @param textInp1 Text que inicialment omple el primer camp
+     * @param options Opcions resultants a mostrar
+     * @return Retorna els resultats dels camps a omplir
+     */
     public  static SimpleEntry<String,String> twoInputDialog(String titol, String label1, String label2, String textInp1, Object[] options) {
 
         //Crear panel inputs
@@ -46,17 +67,31 @@ public class VistaDialeg {
         return null;
     }
 
+    /**
+     * Metode que crea un dialeg per mostrar un error
+     * @param error Text que mostra el dialeg
+     */
     public static void errorDialog(String error) {
         //Crear dialog error
         JOptionPane.showMessageDialog(null, error,
                 "Error detectat", JOptionPane.ERROR_MESSAGE);
     }
 
+    /**
+     * Metode que crea un dialeg per mostrar un missatge
+     * @param titol Titol del dialeg
+     * @param missatge Text del missatge a mostrar
+     */
     public static void messageDialog(String titol, String missatge) {
         //Crear messageDialog
         JOptionPane.showMessageDialog(null, missatge, titol,  JOptionPane.INFORMATION_MESSAGE);
     }
 
+    /**
+     * Funcio que crea un dialeg per confirmar si una accio es vol confirmar
+     * @param missatge Text del missatge que indicara quina accio es vol confirmar
+     * @return Retorna un boolea que indica si es confirma l'accio o no
+     */
     public static Boolean confirmDialog(String missatge){
         //Crear confirm Dialog
         int res = JOptionPane.showConfirmDialog(null, missatge, "Confirmació",
