12fed273d21e00cb28093197422c912b98c21273
pol.verdura <pol.verdura@estudiantat.upc.edu>
2022-12-15 18:43:08 +0100

    documentació vistaAjuda

--- src/Codi/Presentacio/ViewAjuda.java
+++ src/Codi/Presentacio/ViewAjuda.java
@@ -1,35 +1,55 @@
 package Codi.Presentacio;
 
 import javax.swing.*;
-import javax.swing.text.TextAction;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+ * Vista que ajuda a l'usuari a com usar l'aplicacio
+ *
+ * @author pol
+ * @since 15/12/2022
+ */
 public class ViewAjuda {
+
+    ///////////////////////////////////////////////////////////
+    ///                     ESTRUCTURES                     ///
+    ///////////////////////////////////////////////////////////
+
     private JPanel panellSuperior;
     private JPanel panellInferior;
     private JButton tancar_button;
     private JLabel text_ajuda;
     private JFrame frame;
 
+
+    ///////////////////////////////////////////////////////////
+    ///                      FUNCIONS                       ///
+    ///////////////////////////////////////////////////////////
+
+    /**
+     * Creadora per defecte
+     */
     public ViewAjuda() {
         inicialitzarComponents();
         configurarVista();
         assignarListeners();
     }
 
-    private String setText() {
-        return "AJUDA";
-    }
-
+    /**
+     * Inicialitza les estructures de la vista
+     */
     private void inicialitzarComponents() {
         frame = new JFrame("Ajuda");
         panellSuperior = new JPanel();
         panellInferior = new JPanel();
         tancar_button = new JButton("Tancar");
-        text_ajuda = new JLabel(setText());
+        text_ajuda = new JLabel("AJUDA");
     }
 
+    /**
+     * Configura les estructures de la vista
+     */
     private void configurarVista() {
         frame.setLayout(new BorderLayout());
         frame.add(panellSuperior,BorderLayout.NORTH);
@@ -43,6 +63,9 @@ public class ViewAjuda {
         frame.setLocationRelativeTo(null);
     }
 
+    /**
+     * Assigna a les estructures de la vista uns listeners
+     */
     private void assignarListeners() {
         tancar_button.addActionListener(new ActionListener() {
             @Override
@@ -55,10 +78,16 @@ public class ViewAjuda {
         text_ajuda.addKeyListener(new TeclaAjuda());
     }
 
+    /**
+     * Fa que la vista sigui visible
+     */
     public void ferVisible() {
         frame.setVisible(true);
     }
 
+    /**
+     * Configura l'us de les tecles
+     */
     private class TeclaAjuda extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
